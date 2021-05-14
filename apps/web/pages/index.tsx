c5cc3c431121bf39ea9300ec00407112f25c10b1
Pablo Sáez <pablosaez1995@gmail.com>
2021-05-14 09:45:21 -0400

    getserversideprops

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,7 @@
 // import Head from "next/head";
 // import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { GetServerSideProps } from "next";
 
 export default function Home({ name }) {
   return (
@@ -12,8 +13,8 @@ export default function Home({ name }) {
 
 // Remove below and it will work fine!
 
-export async function getServerSideProps(context) {
+export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: { name: "Dan!" },
   };
-}
+};
